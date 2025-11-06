ea91c243f4c997813f680ac8b269a0d927363439
tajima-rd.com <yu_fujimoto@tajima-rd.com>
2025-11-06 20:41:46 +0900

    commit

--- models/drama.py
+++ models/drama.py
@@ -10,7 +10,7 @@ from typing import (
     Any, 
     Optional
 )
-from additional_attribute import AbstractCustomClass
+from .additional_attribute import AbstractCustomClass
 from ..core.config import SceneConfig
 
 class Voice(Enum):
